73a7b0c9ad25c3f7801633f78164e84598beb189
Philipp Bosch <pmbosch94@gmail.com>
2021-01-16 12:06:10 +0100

    final function with extremely informative messages

--- R/show_data_phil.R
+++ R/show_data_phil.R
@@ -7,6 +7,8 @@ library(httr)
 library(purrr)
 library(jsonlite)
 library(magrittr)
+library(tidyr)
+library(stringr)
 
 # define API endpoint
 
@@ -29,12 +31,14 @@ show_data <- function() {
     chuck("result", 1) -> package_list
 
   package_list %>%
-    map_dfr(magrittr::extract, c("id","title")) -> macro_data
+    map_dfr(magrittr::extract, c("id","title"),
+            .id = "datasource") %>%
+    mutate(datasource = as.integer(datasource)) -> macro_data
 
   package_list %>%
     map(chuck, "resources") -> only_ressources
 
-  temp_list <- vector("list", length = length(only_ressources))
+  temp_list_ressources <- vector("list", length = length(only_ressources))
 
   for (i in seq_along(only_ressources)) {
 
@@ -42,12 +46,40 @@ show_data <- function() {
       purrr::chuck(i) %>%
       purrr::map_dfr(magrittr::extract, c("url","format","resource_group_id"),
                      .id = "no_ressource") %>%
-      dplyr::bind_cols(tibble(datasource = i)) -> temp_list[[i]]
+      dplyr::bind_cols(tibble(datasource = i)) -> temp_list_ressources[[i]]
   }
 
-  dplyr::bind_rows(temp_list) -> ressource_df
+  dplyr::bind_rows(temp_list_ressources) -> ressource_df
 
-  return(list(macro_data, ressource_df))
+  package_list %>%
+    map(chuck, "tags") -> only_tags
+
+  temp_list_tags <- vector("list", length = length(only_tags))
+
+  for (i in seq_along(only_tags)) {
+
+    only_tags %>%
+      purrr::chuck(i) %>%
+      purrr::map_dfr(magrittr::extract, c("name"),
+                     .id = "no_tag") %>%
+      dplyr::bind_cols(tibble(datasource = i)) -> temp_list_tags[[i]]
+  }
+
+  dplyr::bind_rows(temp_list_tags) -> tag_df
+
+  tag_df %>%
+    dplyr::mutate(no_tag = stringr::str_c("tag_no", "_",  no_tag)) %>%
+    tidyr::pivot_wider(names_from = no_tag, values_from = name) -> tag_df_merge
+
+  ressource_df %>%
+    dplyr::left_join(macro_data) %>%
+    dplyr::left_join(tag_df_merge) -> global_df
+
+  message("There are in total ", nrow(macro_data), " different datasets available.\n",
+          "These datasets belong to ", nrow(distinct(tag_df, name)), " groups. These groups are:\n",
+          distinct(tag_df, name))
+
+  return(global_df)
 
 }
 
@@ -60,3 +92,11 @@ show_data <- function() {
 function_return <- show_data() # now the function returns two lists in which two dfs are stored
 
 
+
+tag_df %>%
+  dplyr::mutate(no_tag = stringr::str_c("tag_no", "_",  no_tag))
+
+
+function_return %>%
+  group_by(datasource) %>%
+  tally()
