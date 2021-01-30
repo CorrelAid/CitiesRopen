8e4d4c2a7ddba5c6b102df4459a0525a3095a0c9
lukasbirki <73613044+lukasbirki@users.noreply.github.com>
2021-01-30 10:16:10 +0100

    going on

--- R/get_data.R
+++ R/get_data.R
@@ -17,6 +17,7 @@ library(magrittr)
 library(tidyr)
 library(stringr)
 library(rlang)
+library(data.table)
 
 ##Run the show_data function from the first function
 
@@ -28,23 +29,27 @@ source("show_data_phil.R")
 function_return2 <- function_return[1:2,]
 
 function_return %>%
-  select(url,title, format) -> lst
+  select(url,title, name, format) -> lst
 
-lst = lst[1:2,]
-
-basename(lst$url[1])
+lst = lst[4:10,]
 
 ## Setting up Function
 
 get_data <- function(data){
   for (i in 1:nrow(data)){
     url <- data$url[i]
-    name <- basename(lst$url[1]) ##Alternatively, one could also use the respective title in the "title" column
-    utils::download.file(url, "temp.csv")
-    file.rename(from = "temp.csv", to = paste(i,name, sep = "." ))
-  }}
+  if (data$format[i] == "csv"){
+    basename <- data.table::fread(url) %>% ##For CSV Files https://www.rdocumentation.org/packages/data.table/versions/1.13.6/topics/fread
+    assign(paste(data$name[i]),.,envir = .GlobalEnv)}
 
-##Checking
+    #else if(data$format[i] == "xx")
+    #(message("Oh no"))
+  }
+  message("\n","A total number of ", nrow(data), " files were added to your environment")
+}
+
+##Getting data
 lst %>%
   get_data()
+
 
