96ba0680f972328274476f5b291becd4bb3236c1
orpierres <oriane.pierres@uni-konstanz.de>
2021-02-22 18:12:57 +0100

    Filter all columns starting with tag_no

--- R/show_data.R
+++ R/show_data.R
@@ -18,63 +18,63 @@ library(stringr)
 
 show_data <- function(tag = NULL) {
   url <- "https://offenedaten-konstanz.de/api/3/action/current_package_list_with_resources"
-
+  
   resp <- httr::GET(url)
   if (http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-
+  
   parsed <- httr::content(resp, "text") %>%
     jsonlite::fromJSON(simplifyVector = FALSE, flatten = TRUE)
-
+  
   parsed %>%
     chuck("result", 1) -> package_list
-
+  
   package_list %>%
     map_dfr(magrittr::extract, c("id","title"),
             .id = "datasource") %>%
     mutate(datasource = as.integer(datasource)) -> macro_data
-
+  
   package_list %>%
     map(chuck, "resources") -> only_ressources
-
+  
   temp_list_ressources <- vector("list", length = length(only_ressources))
-
+  
   for (i in seq_along(only_ressources)) {
-
+    
     only_ressources %>%
       purrr::chuck(i) %>%
       purrr::map_dfr(magrittr::extract, c("url","name","format","resource_group_id"),
                      .id = "no_ressource") %>%
       dplyr::bind_cols(tibble(datasource = i)) -> temp_list_ressources[[i]]
   }
-
+  
   dplyr::bind_rows(temp_list_ressources) -> ressource_df
-
+  
   package_list %>%
     map(chuck, "tags") -> only_tags
-
+  
   temp_list_tags <- vector("list", length = length(only_tags))
-
+  
   for (i in seq_along(only_tags)) {
-
+    
     only_tags %>%
       purrr::chuck(i) %>%
       purrr::map_dfr(magrittr::extract, c("name"),
                      .id = "no_tag") %>%
       dplyr::bind_cols(tibble(datasource = i)) -> temp_list_tags[[i]]
   }
-
+  
   dplyr::bind_rows(temp_list_tags) -> tag_df
-
+  
   tag_df %>%
     dplyr::mutate(no_tag = stringr::str_c("tag_no", "_",  no_tag)) %>%
     tidyr::pivot_wider(names_from = no_tag, values_from = name) -> tag_df_merge
-
+  
   ressource_df %>%
     dplyr::left_join(macro_data) %>%
     dplyr::left_join(tag_df_merge) -> global_df
-
+  
   
   ##Filter out datasets with their tag
   if (is.null(tag)) {  # Check if there is a filter
@@ -84,12 +84,12 @@ show_data <- function(tag = NULL) {
     return(global_df) # If there is no filter, the function returns all the datasets
   } else {
     global_df %>%
-      dplyr::filter(tag_no_1 %in% tag | tag_no_2 %in% tag | tag_no_3 %in% tag) -> filtered_global #Checks the specified filter in all levels of tags
-      message("You have used the filter(s) ", paste(tag, collapse = " and "), ".") #Indicate whether one or more filters have been specified. 
-      for (i in 1:length(tag)){
-        message("There are in total ", nrow(filter(filtered_global, tag_no_1 == tag [i]| tag_no_2 %in% tag [i] | tag_no_3 %in% tag[i])), 
-                " datasets under the category ", tag[i], ".")
-      }
+      dplyr::filter(if_any(starts_with("tag_no"), ~. %in% tag)) -> filtered_global #Checks the specified filter in all columns containing tags
+    message("You have used the filter(s) ", paste(tag, collapse = " and "), ".") #Indicate whether one or more filters have been specified. 
+    for (i in 1:length(tag)){
+      message("There are in total ", nrow(filter(filtered_global, tag_no_1 == tag [i]| tag_no_2 %in% tag [i] | tag_no_3 %in% tag[i])), 
+              " datasets under the category ", tag[i], ".")
+    }
     return(filtered_global) # Returns only the datasets from the filter
   }     
 }
