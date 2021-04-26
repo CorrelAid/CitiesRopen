1040e4654093152cd75f3c39ac050d2fa6e11f80
lukasbirki <73613044+lukasbirki@users.noreply.github.com>
2021-04-26 22:18:45 +0200

    uptdates

--- R/get_data.R
+++ R/get_data.R
@@ -4,101 +4,86 @@
 ##  Basic Introduction to Programming with dplyr: https://cran.r-project.org/web/packages/dplyr/vignettes/programming.html
 ##  Hands-on Tutorial for writing function in tidyverse: http://jonthegeek.com/2018/06/04/writing-custom-tidyverse-functions/
 
-### get_data ###
+# ### get_data ###
+#
+# # load packages
+# library(dplyr)
+# library(httr)
+# library(purrr)
+# library(jsonlite)
+# library(magrittr)
+# library(tidyr)
+# library(stringr)
+# library(rlang)
+# library(data.table)
+#
+# ##Run the show_data function from the first function
+#
+# rm(list = ls())
+#
+# source("show_data.R")
+#
+# ##Seting up an example list of URLs, which should be provided by the show_data()-Function!
+#
+# function_return %>%
+#   select(url,title, name, format) %>%
+#   .[28:38,]-> lst
 
-rm(list = ls())
 
-# load packages
-library(dplyr)
-library(httr)
-library(purrr)
-library(jsonlite)
-library(magrittr)
-library(tidyr)
-library(stringr)
-library(rlang)
-library(data.table)
-
-##Run the show_data function from the first function
-
-rm(list = ls())
-
-source("show_data.R")
-
-##Seting up an example list of URLs, which should be provided by the show_data()-Function!
-
-function_return2 <- function_return[1:2,]
-
-function_return %>%
-  select(url,title, name, format) -> lst
-
-lst = lst[4:10,]
-
-get_data <- function(data, download = "single"){
-  answer <- readline(prompt=message("If you continue, a total of ",nrow(data)," files are downloaded.\nDo you want to proceed? (Y/N)? \nPlease type the correct letter into the console and execute!"))
-  if (answer == "Y" | answer == "y" | answer == "Yes" | answer == "yes"){
-    if (download == "Single" | download == "S"){
+#' Get Data Function (no final description)
+#'
+#' @param data The data specified in the show_data function
+#' @param download Specifies the way, how the data is downloaded. Choose from c("Environment", "Local")
+#'
+#' @return Returns the data from the
+#' @export
+#'
+#' @examples show_date(tag = 'XX') %>% get_data()
+get_data <- function(data, download = "Environment"){
+  answer <- readline(prompt=message("If you continue, a total of ",nrow(data)," files will be downloaded.\nDo you want to proceed? (Y/N)? \nPlease type the correct letter into the console and execute!"))
+  # 1. Step: Stop, if there is no permission to download
+  if (answer == "N" | answer == "n" | answer == "No" | answer == "no"){
+    stop("You have aborted the download. Please run the function again!")}
+  # 2. Step: Evaluate download option and start downloading
+  if (download == "Environment" | download == "E"){
+      assign("List_Open_Data",list(), envir = .GlobalEnv)
       purrr::pwalk(.l = data,.f = function(...){
         current <- tibble(...)
+        #CSV: Define and Apply Fetcher
         if (current$format == "csv"){
-          basename <- data.table::fread(current$url) %>%  ##For CSV Files https://www.rdocumentation.org/packages/data.table/versions/1.13.6/topics/fread
-          assign(paste(current$name),.,envir = .GlobalEnv)
+          List_Open_Data <<- append(List_Open_Data, list(data.table::fread(current$url)))
+          names(List_Open_Data)[length(List_Open_Data)] <<- current$name
         }
-        ###Placeholder for subsequent File Formats, e.g.
-        # if (current$format == "txt"){
-        #   ...
-        # }
-      })
-    }
-    else if (download == "Environment" | download == "E"){
-      assign("List_Open_Data_2",list(), envir = .GlobalEnv)
-      purrr::pwalk(.l = data,.f = function(...){
-        current <- tibble(...)
-        if (current$format == "csv"){
-          List_Open_Data_2 <- append(List_Open_Data_2, purrr::map(List_Open_Data_2, data.table::fread))
-          assign("List_Open_Data_2",List_Open_Data_2, envir = .GlobalEnv)
-
+        #JSON: Define and Apply Fetcher
+        else if (current$format == "json") {
+          List_Open_Data <<- append(List_Open_Data, list (jsonlite::fromJSON(txt = current$url, flatten = TRUE)))
+          names(List_Open_Data)[length(List_Open_Data)] <<- current$name
+        }
+        #Others: Append URL for further analysis
+        else {
+          List_Open_Data <<- append(List_Open_Data, list(current$url))
+          names(List_Open_Data)[length(List_Open_Data)] <<- current$name
         }
       })
     }
-    else if (download == "Local" | download == "L"){
+  else if (download == "Local" | download == "L"){
     dir.create("Open_Data_Konstanz")
     purrr::pwalk(.l = data,.f = function(...){
       current <- tibble(...)
       utils::download.file(url = current$url, destfile = paste('./Open_Data_Konstanz/',current$name,'.',current$format, sep = ''), quiet = T)
     })
   }
-    else (message("You have not specified a proper setting for download. Please come back later or change the settings"))
-   } else message("You have aborted the download. Please come back later or change the settings")
-  }
+}
+
+
+### Working version
+
+##Testing
+# lst %>% get_data()
 
 
 
-lst %>%
-  get_data(download = 'L')
-
-lst_new <- lst[3:5,]
 
 
-y <- purrr::pwalk(.l = lst_new,.f = function(...){
-  current <- tibble(...)
-  if (current$format == "csv"){
-    List_Open_Data_2 <- append(list(List_Open_Data_2), purrr::map(current$url, data.table::fread))
-    return(List_Open_Data_2)
-  }
-})
 
-
-List_Open_Data <-
-append(List_Open_Data,purrr::pmap(.l = lst_new,.f = function(...){
-current <- tibble(...)
-data <- data.table::fread(current$url)
-}))
-
-leng(List_Open_Data)
-purrr::pwalk(.l = lst_new,.f = function(...){
-data <- list(data.table::fread(lst_new$url[1]))
-List_Open_Data_3 <- list()
-List_Open_Data_3 <- append(List_Open_Data_3, data)
-List_Open_Data[[length(List_Open_Data)+1]] <- data
 
