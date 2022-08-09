7aba273fde4cd60c8fba041c30bccec90ffdaa36
MaFa211 <mats.faulborn@gmx.net>
2022-08-09 10:01:02 +0200

    updating gitignore

--- R/get_data.R
+++ R/get_data.R
@@ -7,49 +7,112 @@
 #' @export
 #'
 #' @examples show_data(tag = 'Umwelt und Klima') %>% get_data()
-get_data <- function(data, download = "Environment"){
+
+get_data <- function(data, download = "Environment")  {
 
   #Asking for permission to download
   cli::cli({helper_message_authorization(data)})
   answer <- readline()
-
+  
   # 1. Step: Stop, if there is no permission to download
   if (answer == "N" | answer == "n" | answer == "No" | answer == "no"){
   cli::cli_alert_danger("You have aborted the download. Please run the function again!")
   helper_stopQuietly()}
-  # 2. Step: Evaluate download option and start downloading
+  
+  # 2. Validate URLs. Problem with this implementation: We request every URL twice.
+  #Could rewrite get_data function -> request URL once, if URL is valid, process the HTTP body dependend on current$format
+  working_urls <- sapply(data$url, RCurl::url.exists, USE.NAMES = FALSE)
+  
+  #Number of excluded datasets for cli message
+  excluded_data <- data[!working_urls,]
+  x = nrow(excluded_data)
+  
+  #Excluding invalid urls
+  data <- data[working_urls,]
+  
+  #Replace nas of missing formats
+  for(i in 1:nrow(data)){
+    if(is.na(data$format[i])){
+      data$format[i] <- "unknown"
+      x = x+1
+    }
+  }
+  
+  #May include some info about excluded urls
+  #Need for documentation
+  id <- cli::cli_status("")
+  cli::cli_alert_info("We have exluded {x} datasets because of invalid URLs or unknown fomats")
+  
+  
+  # 4. Step: Evaluate download option and start downloading
   if (download == "Environment" | download == "E"){
 
     assign("List_Open_Data",list(), envir = .GlobalEnv)
-
+    assign("remaining_data",list(), envir = .GlobalEnv)
+    
     helper_message_start_download()
-    id <- cli::cli_status("")
+    #id <- cli::cli_status("")
+
 
     purrr::pwalk(.l = data,.f = function(...){
       current <- tibble::tibble(...)
       #CSV: Define and Apply Fetcher
       if (current$format == "csv"){
         cli::cli_status_update(id,"Downloading {current$name} of type {.emph {current$format}}")
-        List_Open_Data <<- append(List_Open_Data, list(data.table::fread(current$url, showProgress = F, encoding = "UTF-8",)))
+        List_Open_Data <<- append(List_Open_Data, list(data.table::fread(current$url, showProgress = F, encoding = "UTF-8")))
         names(List_Open_Data)[length(List_Open_Data)] <<- current$name
         cli::cli_status_update(id, cli::cli_alert_success("{current$name}: {cli::symbol$tick} Done"))
       }
       #JSON: Define and Apply Fetcher
-      else if (current$format == "json") {
-        cli::cli_status_update(id,"Downloading {current$name} of type {emph. {current$format}}")
+      else if (current$format == "json" | current$format == "geojson") {
+        cli::cli_status_update(id,"Downloading {current$name} of type {.emph {current$format}}")
         List_Open_Data <<- append(List_Open_Data, list (jsonlite::fromJSON(txt = current$url, flatten = TRUE)))
         names(List_Open_Data)[length(List_Open_Data)] <<- current$name
-        cli::cli_status_update(id, cli::cli_alert_success("{current$name}:Done"))
+        cli::cli_status_update(id, cli::cli_alert_success("{current$name}: {cli::symbol$tick} Done"))
       }
+      #Excel: Define and Apply Fetcher
+      else if (current$format == "xls" | current$format == "xlsx") {
+        cli::cli_status_update(id,"Downloading {current$name} of type {.emph {current$format}}")
+        List_Open_Data <<- append(List_Open_Data, list (rio::import(current$url)))
+        names(List_Open_Data)[length(List_Open_Data)] <<- current$name
+        cli::cli_status_update(id, cli::cli_alert_success("{current$name}: {cli::symbol$tick} Done"))
+      }
+      #XML: Define and Apply Fetcher
+      else if (current$format == "xml") {
+        cli::cli_status_update(id,"Downloading {current$name} of type {.emph {current$format}}")
+        List_Open_Data <<- append(List_Open_Data, list (XML::xmlTreeParse("{current$url}")))
+        names(List_Open_Data)[length(List_Open_Data)] <<- current$name
+        cli::cli_status_update(id, cli::cli_alert_success("{current$name}: {cli::symbol$tick} Done"))
+      } 
       #Others: Append URL for further analysis
-        else {
-          List_Open_Data <<- append(List_Open_Data, list(current$url))
-          names(List_Open_Data)[length(List_Open_Data)] <<- current$name
-          #helper_message_unknown_format(current$name, current$format)
+      else {
+        # List_Open_Data <<- append(List_Open_Data, list(current$url))
+        # names(List_Open_Data)[length(List_Open_Data)] <<- current$name
+        # temp <- data.frame(url = current$url, format = current$format, name = current$name)
+        # remaining_data <- dplyr::bind_rows(remaining_data, temp)
+        # return(remaining_data)
+        remaining_data <<- append(remaining_data, list(c(current$url, current$format, current$name)))
+        
+        
         }
-      })
+    })
+    
+    if (length(remaining_data>0)){
+      cli::cli_alert_info("The remaining {length(remaining_data)} datasets cannot be loaded in the R Environment. Do you want to save them locally? (Y/N)")
+      answer <- readline()
+      if ("Y" %in% answer | "y" %in% answer) {
+        dir.create("Open_Data_Remaining")
+        purrr::pwalk(.l = remaining_data,.f = function(...){
+          utils::download.file(url = current$url, destfile = paste('./Open_Data_Remaining/',current$name,'.',current$format, sep = ''), quiet = T)
+        })}
+      else {
+        cli::cli_alert_info("The remaining data is dismissed.")
+      }
     }
+  }
+  
   else if (download == "Local" | download == "L"){
+    
     dir.create("Open_Data_Konstanz")
     purrr::pwalk(.l = data,.f = function(...){
       current <- tibble::tibble(...)
@@ -59,7 +122,6 @@ get_data <- function(data, download = "Environment"){
   cli::cli_status_clear(id)
   helper_message_end_download()
 }
-
 
 
 
